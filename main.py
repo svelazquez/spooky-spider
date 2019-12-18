0a49764adb2a8c78cc65373ad57d38ce204d3c6b
Stephanie Velazquez <velazquez.stephanie02@gmail.com>
2019-12-17 23:10:08 -0600

    use the right selector. all ids under span that have quote in them

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Run: $ scrapy runspider main.py"""
+import os
 import scrapy
 
 class SpiderKun(scrapy.Spider):
@@ -14,23 +15,11 @@ class SpiderKun(scrapy.Spider):
         except KeyError as e:
             print(f"{e}: Cannot split url into pages. Check url.")
 
-        filename = 'quotes-%s.html' % page
-        # TODO truncate file if not empty and is first iteration. 
+        filename = "quotes.html"
         with open(filename, 'a') as f:
-            stuff = [f.write(res) for res in response.css(f'span#quote_display_content').getall()]
-            print(f"Got this stuff for you: {stuff}")
-            # wtf is this stuff? id's? 
-            # stuff = [76, 32, 59, 17, 75, 30, 73, 31, 73, 31, 63, 19, 78, 33, 62, 
-            # 20, 69, 29, 82, 38, 74, 29, 98, 69, 74, 23, 51, 33, 41, 68, 
-            # 65, 53, 40, 310, 106, 46, 53, 40, 166, 103, 46, 53, 40, 436,
-            #  103, 46, 53, 40, 508, 103, 46, 53, 40, 553, 103, 46, 53, 40, 
-            # 390, 101, 46, 53, 40, 198, 101, 46, 53, 40, 832, 101, 46, 53, 
-            # 40, 521, 101, 46, 53, 40, 521, 101, 46, 53, 40, 253, 101, 46, 
-            # 53, 40, 232, 101, 46, 53, 40, 301, 101, 46, 53, 40, 419, 101, 
-            # 46, 53, 40, 489, 101, 46, 53, 40, 355, 100, 46, 53, 40, 423, 
-            # 100, 46, 53, 40, 461, 100, 46, 53, 40, 2159, 100, 46, 53, 40, 
-            # 625, 100, 46, 35]
-        self.log('Saved file %s' % filename)
+            stuff = [f.write(res) for res in response.css("span[id*='quote_display_content']").getall()]
+        self.log(f"Saved file {filename}")
+        
         # identify next page and recurse
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
