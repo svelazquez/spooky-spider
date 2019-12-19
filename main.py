685143f020c98b586718eaa240b7c41e6ef76111
sv <velazquez.stephanie02@gmail.com>
2019-12-18 23:15:39 -0600

    read from file in spider dir, also linting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Run: $ scrapy runspider main.py"""
 import os
+import re
+
 import scrapy
 
 class SpiderKun(scrapy.Spider):
@@ -17,11 +19,28 @@ class SpiderKun(scrapy.Spider):
 
         filename = "quotes.html"
         with open(filename, 'a') as f:
-            stuff = [f.write(res) for res in response.css("span[id*='quote_display_content']").getall()]
+            #memeing filtered_row
+            filtered_row = [re.sub(r'\<.*\>', ',', res) for res in response.css("span[id*='quote_display_content']").getall()]
+            print(filtered_row)
+            for r in filtered_row:
+                print(r)
+                f.write(r)
+
+
         self.log(f"Saved file {filename}")
         
+        # # TODO fix the dumb
+        # self.clean(filename) 
+
+
         # identify next page and recurse
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+    
+
+    # def clean(self, filename):
+    #     with open(filename, 'a') as f:
+    #         f.write(re.sub(r'\<.*\>', '', res))
+            
